dfb497b60a4b4901c4dae95c3db276a007688d1b
ivcoder <vank300828@gmail.com>
2025-09-19 22:53:45 +0200

    upd test

--- test.py
+++ test.py
@@ -1,11 +1,11 @@
 from avl_node import insert_avl
-from lib import min_value_node
+from lib import get_min_node_value
 
 print("=== AVL Tree Example ===")
 root = None
-values_to_insert = [10, 20, 30, 40, 50, 25]
+values_to_insert = [10, 20, 5, 8, 3, 4, 1]
 for value in values_to_insert:
     root = insert_avl(root, value)
     print(f"Inserted {value} into AVL Tree.")
     print(root)
-    print("Min value node:", min_value_node(root))
+    print("Min node value:", get_min_node_value(root))
